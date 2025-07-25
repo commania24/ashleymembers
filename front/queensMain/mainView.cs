c5a21cfe210a3e0d42b4e452d35f65e495f5e7be
commania24 <commania24@gmail.com>
2025-07-25 16:13:52 +0900

    Create mainView.cs

---
+++ front/queensMain/mainView.cs
@@ -0,0 +1,10 @@
+<!-- mainView.cs -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=mainView_cs/index.html" />
+  </head>
+  <body>
+    <p>Redirecting...</p>
+  </body>
+</html>
